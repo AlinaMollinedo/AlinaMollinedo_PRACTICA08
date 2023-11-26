578f44fdc47b2617c7060b61ba69bc629cc002a1
x0o0-x <alina.mollinedo@ucb.edu.bo>
2023-11-25 23:27:35 -0400

    Completados ejs 1 y 2

---
+++ Ejercicio_08_01.cpp
@@ -0,0 +1,134 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor: Alina Mollinedo Dávila
+
+// Fecha creación: 25/11/2023
+
+// Fecha modificación: 25/11/2023
+
+// Número de ejericio: 1
+
+// Problema planteado: Escribir un programa con la opción de encriptar y de desencriptar un fichero de texto.
+// La encriptación consiste en que dado un fichero de texto de entrada genere otro fichero de
+// salida de extensión <nombre>.COD donde el texto estará codificado (encriptado).
+// Esta codificación consiste en reemplazar cada carácter por el tercero siguiente según la tabla
+// ASCII.
+// La opción de desencriptado consiste en leer un fichero <nombre>.COD y recuperar la
+// información original.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const char* fileNormal = "Data.txt";
+const char* fileEncrypted = "Data.COD";
+
+void generateFile();
+void encryptFile();
+void decryptFile();
+void readEncryption();
+
+int main(){
+    ifstream rf;
+    rf.open(fileNormal);
+    if(rf.good()){
+        int op;
+        cout << "Desea agregar informacion al archivo?" << endl;
+        cout << "[1] SI\t[2] NO\t>> ";
+        cin >> op;
+
+        if(op == 1){
+            generateFile();
+        }
+    }
+    else{
+        generateFile();
+    }
+    rf.close();
+
+    int op;
+    cout << "\nDesea encriptar el archivo o desencriptar el archivo?" << endl;
+    cout << "\n[1] ENCRIPTAR\t[2] DESENCRIPTAR\t>> ";
+    cin >> op;
+
+    if(op == 1){
+        encryptFile();
+        readEncryption();
+    }
+    else{
+        decryptFile();
+    }
+    return 0;
+}
+
+void generateFile(){
+    ofstream wf;
+    string str;
+
+    wf.open(fileNormal, ios::app);
+    cout << "\nIngrese la informacion a guardar en el archivo: " << endl;
+    cin.ignore();
+    getline(cin, str);
+    wf << str << endl;
+    wf.close();
+}
+
+void encryptFile(){
+    ofstream wf;
+    ifstream rf;
+    wf.open(fileEncrypted);
+    rf.open(fileNormal);
+
+    string str;
+    while(!rf.eof()){
+        getline(rf, str);
+        int sizeofline = str.length();
+        for(int i = 0; i < sizeofline; i++){
+            int s = int(str[i]) + 3;
+            wf << static_cast<char>(s);
+        }
+        wf << endl;
+    }
+    rf.close();
+    wf.close();
+}
+
+void decryptFile(){
+
+    ifstream rf;
+    rf.open(fileEncrypted);
+
+    if(rf.good()){
+        cout << "\n\nARCHIVO DESENCRIPTADO: " << endl;
+        cout << "=======================\n" << endl;
+        string str;
+        while(!rf.eof()){
+            getline(rf, str);
+            int sizeofline = str.length();
+            for(int i = 0; i < sizeofline; i++){
+                int s = int(str[i]) - 3;
+                cout << static_cast<char>(s);
+            }
+            cout << endl;
+        }
+        rf.close();
+    }
+    else{
+        cout << "\nNo se ha encriptado nada todavia." << endl;
+    }
+    
+}
+
+void readEncryption(){
+    cout << "\n\nARCHIVO ENCRIPTADO: " << endl;
+    cout << "==================\n" << endl;
+    ifstream rf;
+    rf.open(fileEncrypted);
+    while(!rf.eof()){
+        string str;
+        getline(rf, str);
+        cout << str << endl;
+    }
+}
---
+++ Ejercicio_08_02.cpp
@@ -0,0 +1,62 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor: Alina Mollinedo Dávila
+
+// Fecha creación: 25/11/2023
+
+// Fecha modificación: 25/11/2023
+
+// Número de ejericio: 2
+
+// Problema planteado: Escriba un programa que cree un fichero de texto llamado "PERSONAS.BIN" en el que se
+// guarde la información de un número indeterminado de personas.
+// La información que se guardará por cada persona será:
+// • Nombre: De 1 a 30 caracteres.
+// • Edad numérico (>= 1 y <=100)
+// • Sexo CHAR (M/F).
+// • FechaNacimiento CHAR(10) (formato dd/mm/yyyy)
+// La información correspondiente a cada persona se leerá del teclado.
+// El proceso finalizará cuando se teclee un campo "Nombre" que esté solamente con el carácter
+// de espacio.
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+const char* file = "PERSONAS.BIN";
+
+void addFile(char[30]);
+
+int main(){
+    char name[30], n[30] = " ";
+    do{
+        cout << "\nNombre: ";
+        cin.getline(name, 31);
+        if(strcmp(name, n) == 0){
+            break;
+        }
+        addFile(name);
+    }
+    while(name != n);
+    return 0;
+}
+
+void addFile(char name[30]){
+    char gender, birthday[10];
+    int age;
+    cout << "Edad: ";
+    cin >> age;
+    cin.ignore(256, '\n');
+    cout << "Sexo: ";
+    cin >> gender;
+    cin.ignore(256, '\n');
+    cout << "Fecha de nacimiento (dd/mm/yyyy): ";
+    cin.getline(birthday, 11);
+
+    ofstream wf;
+    wf.open(file, ios::app);
+    wf << name << age << gender << birthday << endl;
+    wf.close();
+}
